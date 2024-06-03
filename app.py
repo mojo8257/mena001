ad7b4ffb3481388813f294def04f99f5de43bda4
Amos <3103643264@qq.com>
2024-06-03 21:12:13 +0800

    init

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
+import logging
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+app.logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler('flask_app.log')
+file_handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+
+app.logger.addHandler(file_handler)
 
 @app.route('/')
 def home():
